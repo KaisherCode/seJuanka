8b8fbd726a908412a2ab2eb1fefa8704dcf4d1b9
Wilson Dev <wilson.hch@hynkor.com>
2023-08-03 12:01:27 -0500

    Implementación de botones descargar cv y link a linkedIn y mejoras al componente Home

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,16 +1,19 @@
+import white_papper from '../../assets/white_papper.pdf'
+import { FaLinkedin,FaArrowCircleDown } from 'react-icons/fa'
 import home from '../../assets/image-home.png'
-const Home = () => {
+
+const Home = ({ name, title,linkedIn }) => {
   return (
     <section id="home" className=" min-h-screen flex justify-evenly items-center">
       <div className=" abolute ">
         <h1 className="mb-4 font-bold text-3xl">Hello everyone!</h1>
-        <h2 className="mb-4">My name is <span className=" font-semibold text-lg">Kaisher Code</span>, I'm a Full Stack Developer</h2>
+        <h2 className="mb-4">My name is <span className=" font-semibold text-lg text-red-400">{name}</span>, {title}</h2>
         <div className="flex gap-2">
-        <a href="" className="p-2 border-2 border-indigo-500 rounded-lg hover:bg-slate-200 " >Check my CV</a>
-        <a href="" className="p-2 border-2 border-indigo-500 rounded-lg hover:bg-slate-200 ">LinkedIn</a>
+          <a href={white_papper} download className="p-2 border-2 border-indigo-500 rounded-lg hover:bg-red-400 flex gap-2 justify-evenly items-center " ><FaArrowCircleDown/>Download CV</a>
+          <a href={linkedIn} target='_blank' className="p-2 border-2 border-indigo-500 rounded-lg hover:bg-red-400 flex gap-2 justify-evenly items-center ">LinkedIn<FaLinkedin /></a>
         </div>
       </div>
-      <img className="w-96 h-96 rounded-full"  src={home} alt="" />
+      <img className="w-96 h-96 rounded-full" src={home} alt="" />
     </section>
   )
 }
--- src/app.jsx
+++ src/app.jsx
@@ -6,16 +6,27 @@ import { Project } from './Components/Project'
 import { Footer } from './Components/Footer'
 import './app.css'
 
+const siteProps = {
+name:"Kaisher Code",
+title:"I'm Full Stack Developer",
+email:"web@hynkor.com",
+github:"@KaisherCode",
+instagram: "@Kaishercode",
+linkedIn: "https://www.linkedin.com/in/freddy-wilson-h-1788289b",
+twitter: "@KaisherCode",
+tiktok: "KaisherCode",
+}
+
 export function App() {
 
   return (
     <div id='main' className=' w-screen'>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Skills/>
-      <Project/>
-      <Footer/>
+      <Navbar />
+      <Home name={siteProps.name} title={siteProps.title} linkedIn={siteProps.linkedIn} />
+      <About />
+      <Skills />
+      <Project />
+      <Footer />
     </div>
   )
 }
