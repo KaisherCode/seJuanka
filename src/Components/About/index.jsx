9d705a7d51a1327eac6139ca76eac143d093296d
Wilson Dev <wilson.hch@hynkor.com>
2023-08-04 15:13:12 -0500

    mejoras al componente About

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,38 +1,15 @@
 
+const description = "I am a professional in Computer Science and Informatics. I like to build attractive applications and websites both backend and frontend, using Javascript, TailwindCss, React.js/Next.js and Node. I am also familiar with graphic design for creating logos and icons using Adobe Illustrator."
+const detailOrQuote = "Learning new things always excites me, and I am constantly looking for new challenges to expand my knowledge and skills."
 const About = () => {
   return (
-    <section id="about" className=" dark:text-white pl-52 pr-52 flex justify-evenly items-center relative w-screen">
+    <section id="about" className=" dark:text-white pl-52 pr-52 flex justify-evenly  ">
       <div className=" border-2 rounded-lg p-16 ">
-        <div className="p-16">
-        <h2 className=" text-center font-bold text-2xl pt-0 mb-6 ">About Myself</h2>
-        <p className=" text-3xl font-light text-center mb-4">I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways. </p>
-        </div>
-        <hr />
-        <ul className="grid grid-cols-2 mb-4 gap-6 p-2 pr-10 pl-10">
-          <li>
-            <a href="">Web design</a>
-          </li>
-          <li>
-          User experience
-          </li>
-          <li>
-          Inclusive design
-          </li>
-          <li>
-          Focus group testing
-          </li>
-          <li>
-          Mobile user interfaces
-          </li>
-          <li>
-          Graphic design
-          </li>
-        </ul>
-        <hr/>
-        <p className=" text-center font-light text-2xl p-16">
-          I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.
+        <h2 className=" font-bold text-2xl mb-6 text-center ">About Myself</h2>
+        <p className=" text-2xl font-light">{description}</p>
+        <p className="  font-light text-2xl">
+          {detailOrQuote}
         </p>
-
       </div>
     </section>
   )
