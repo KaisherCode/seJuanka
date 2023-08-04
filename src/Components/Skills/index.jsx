0c3fd36749897d133565a9102678c53ed6ae1470
Wilson Dev <wilson.hch@hynkor.com>
2023-08-04 13:23:26 -0500

    Mejoras en el componente Skills

--- src/Components/Skills/index.jsx
+++ src/Components/Skills/index.jsx
@@ -1,60 +1,54 @@
-import {FaCss3, FaHtml5,FaPython,FaJs,FaReact,FaGit,FaGithub,FaNode} from 'react-icons/fa'
+
+const skillList = [{
+  name: "Html",
+  image: "http://drive.google.com/uc?export=view&id=1LAIPYIup-Yg7ie4Q2NQMCguvt5goN01-",
+},
+{
+  name: "Css",
+  image: "http://drive.google.com/uc?export=view&id=1v0ukuA1yf9nYt6H6ImI7iz5Hz9Q7Ojf6",
+},
+{
+  name: "Javascript",
+  image: "http://drive.google.com/uc?export=view&id=1pFXNpp8w6p_cxbymxfshHO-VVVYsF2rp",
+},
+{
+  name: "React js",
+  image: "http://drive.google.com/uc?export=view&id=1XuffJnevUp0g7_qM4ck3wNIQPA8Nc0Lj"
+},
+{
+  name: "Node js",
+  image: "http://drive.google.com/uc?export=view&id=1s2G568hsGRHkZoID5wqnPT-DXx010Gfc",
+},
+{
+  name: "GitHub",
+  image: "http://drive.google.com/uc?export=view&id=1-9ETB8nW2CQEE8Bo61JXT_WCG1nnakR5",
+},
+{
+  name: "MySql",
+  image: "http://drive.google.com/uc?export=view&id=1JgU1pausdUwof8kZLe-YMQCjqLFCt7S7",
+},
+{
+  name: "Git",
+  image: "http://drive.google.com/uc?export=view&id=1oR59RsjbHKFS3UL-oS8p_t6qqueAIECx",
+},
+]
+
 const Skills = () => {
   return (
-    <div id='skills' className='flex flex-col justify-evenly items-center dark:text-white'>
-      <h1 className="  text-center font-bold text-2xl pt-16 mb-4 ">My Skills</h1>
-      <p className=" text-center font-light text-2xl mb-4 ">Working code using languages: JavaScript, HTML, CSS, MySql, Postgres AND Python</p>
+    <section id='skills' className='flex flex-col justify-evenly items-center dark:text-white'>
+      <h2 className="  text-center font-bold text-2xl pt-16 mb-4 ">My Skills</h2>
+      <p className=" text-center font-light text-2xl mb-4 ">Working code using languages: HTML, CSS, JavaScript, React.js, Node.js, GitHub, MySql and Git</p>
       <div className='grid gap-4 grid-cols-4 w-auto max-w-screen-lg mb-10'>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
+      {skillList.map((skill) => (
+        <div className=" cursor-pointer w-full h-50 rounded-lg shadow-2xl border hover:border-indigo-500">
+            <figure className="relative  p-8">
+              <img className="w-full h-full object-cover rounded-lg mb-2" src={skill.image} alt={skill.name} />
+              <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-indigo-200 rounded-lg text-black text-sx m-2 px-3 py-0.5">{skill.name}</span>
+            </figure>
         </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
-        <div className="bg-white cursor-pointer w-full h-50 rounded-lg shadow-2xl border-2 hover:border-indigo-500">
-          <figure className="relative mb-1 w-full h-full">
-            <span className="flex justify-center items-center gap-2 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Html<FaHtml5 /></span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
-          </figure>
-        </div>
+        ))}
       </div>
-    </div>
+    </section>
   )
 }
 export { Skills }
